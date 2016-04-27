e41e817b0729bb26da0649f505d7ba8c67f154ca
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2016-04-27 18:21:05 +0530

    Initail commit and header section added

---
+++ README.md
@@ -0,0 +1,37 @@
+# Developer Starter Kit(developer-starter-kit)
+
+### Features
+This starter kit consist of following tools which wil make your development quick
+
+* Jade (HTML Templating Language)
+* SASS (CSS preprocessor)
+* Gulp Task Runner
+* Bower Package Manager
+
+### Future amendments
+* AngularJS
+* Bootstrap
+
+
+### Installation
+Following are the steps to install this developer starter kit on your local.
+
+- First clone it from the github repository 
+
+```sh
+$ git clone https://github.com/imparvez/developer-starter-kit.git
+```
+
+- Then download all the node packages with npm install command
+```sh
+$ npm install
+```
+
+- There is a default Jquery dependency which can be download from bower using
+```sh
+$ bower install
+```
+
+
+### Version
+0.0
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var	uglify = require('gulp-uglify');
+var	jade = require('gulp-jade');
+var	sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+
+//Concat and Minify
+gulp.task('concat', function(){
+	return gulp.src('dev/script/*.js')
+			.pipe(concat('main.js'))
+			.pipe(rename({suffix:'.min'}))
+			.pipe(uglify())
+			.pipe(gulp.dest('prod/assets/script/'));
+});
+
+//Sass
+gulp.task('sass', function(){
+	return sass('dev/css/style.scss', { style: 'expanded' })
+					.pipe(autoprefixer({
+						browsers: ['last 4 versions'],
+						cascade: false
+					}))
+        	.pipe(gulp.dest('prod/assets/css/'))
+});
+
+//Jade Template
+gulp.task('template', function(){
+	gulp.src('dev/template/index.jade')
+		.pipe(jade({
+			pretty: true
+		}))
+		.pipe(gulp.dest('prod/'));
+});
+
+//BrowserSync Task
+gulp.task('sass-watch', ['sass'], browserSync.reload);
+gulp.task('template-watch', ['template'], browserSync.reload);
+
+
+//Watch Task
+gulp.task('watch', function(){
+	browserSync({
+		server: {
+			baseDir: 'prod/'
+		}
+	})
+	gulp.watch('dev/css/style.scss', ['sass-watch']);
+	gulp.watch('dev/template/*.jade', ['template-watch']);
+})
+
+
+gulp.task('default', ['concat','watch']);
+
+
+
+
+
+
+
+
+
